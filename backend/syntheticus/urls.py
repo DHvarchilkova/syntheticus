9ec6f1a06a111d4848c6094f09958d126078c920
remko520 <59017536+remko520@users.noreply.github.com>
2020-04-07 08:37:04 +0200

    add admin backend url

--- backend/syntheticus/urls.py
+++ backend/syntheticus/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
