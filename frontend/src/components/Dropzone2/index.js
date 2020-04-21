55624e552d38044dabf4e2043be2958556aa77f0
remko520 <59017536+remko520@users.noreply.github.com>
2020-04-21 18:44:32 +0200

    dataset upload functionality

--- frontend/src/components/Dropzone2/index.js
+++ frontend/src/components/Dropzone2/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import 'react-dropzone-uploader/dist/styles.css'
-import Dropzone from 'react-dropzone-uploader'
+import 'react-dropzone-uploader/dist/styles.css';
+import Dropzone from 'react-dropzone-uploader';
 
 
 const MyUploader = () => {
   // specify upload params and url for your files
-  const getUploadParams = ({ meta }) => { return { url: 'https://syntheticus.propulsion-learn.ch/backend/api/datasets/upload' } }
+  const getUploadParams = ({ meta, file }) => {
+    console.log(file)
+    const token = localStorage.getItem('token')
+    const headers = {Authorization: `Bearer ${token}`}
+    const body = new FormData()
+    body.append('dataset', file)
+    return { url: 'https://syntheticus.propulsion-learn.ch/backend/api/datasets/upload', headers, body } }
 
   // called every time a file's `status` changes
   const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
