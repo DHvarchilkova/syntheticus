cc1c399fb08ec29f65f6f3f0f96cefac4ba93bf2
Desislava Hvarchilkova <dhvarchilkova@gmail.com>
2020-04-22 20:32:54 +0200

    login url fixed

--- frontend/src/store/actions/loginActions.js
+++ frontend/src/store/actions/loginActions.js
@@ -1,32 +1,32 @@
-import axios from 'axios';
+import Axios from '../../axios';
 
 export const userLoginDispatch = token => {
-  return {
-    type: "login",
-    payload: token
-  };
+    return {
+        type: "login",
+        payload: token
+    };
 };
 
 export const userLoginError = error => {
-  return {
-    type: "error",
-    payload: error
-  };
+    return {
+        type: "error",
+        payload: error
+    };
 };
 
-export const userLoginAction = ({ email, password }) => (dispatch, getState) => {
-  return axios.post('http://localhost:8000/backend/api/auth/token', { email, password})
-  .then( response => {
-      dispatch(userLoginDispatch(response.data.access))
-      localStorage.setItem('token', response.data.access)
-      localStorage.setItem('username', response.data.user.username)
-      localStorage.setItem('email', response.data.user.email)
-       console.log('me.username: '+ localStorage.getItem('username'))
-      console.log('me.email: '+ localStorage.getItem('email'))
-      return response
-  }).catch(error => {
-      dispatch(userLoginError(error))
-  })
+export const userLoginAction = ({email, password}) => (dispatch, getState) => {
+    return Axios.post('auth/token', {email, password})
+        .then(response => {
+            dispatch(userLoginDispatch(response.data.access))
+            localStorage.setItem('token', response.data.access)
+            localStorage.setItem('username', response.data.user.username)
+            localStorage.setItem('email', response.data.user.email)
+            console.log('me.username: ' + localStorage.getItem('username'))
+            console.log('me.email: ' + localStorage.getItem('email'))
+            return response
+        }).catch(error => {
+            dispatch(userLoginError(error))
+        })
 }
 
 
