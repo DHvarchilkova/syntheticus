3f15093adfb0c1f2b592897773aef86980ff9ff1
Desislava Hvarchilkova <dhvarchilkova@gmail.com>
2020-04-23 12:33:30 +0200

    processing page fixed

--- frontend/src/components/ChooseOptions/index.js
+++ frontend/src/components/ChooseOptions/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Axios from '../../axios';
 import { connect } from 'react-redux';
 import { useHistory } from "react-router-dom"
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 
 import Body from '../Body';
 import { WhiteButton} from "../../styles/GlobalButtons";
+import Processing from "../Processing";
 
 import stock3 from '../../assets/stock3.jpg';
 import {userLoginAction} from "../../store/actions/loginActions";
@@ -103,61 +104,73 @@ const RowsInput = styled.input`
 `;
 
 
-const ChooseOptions = () => {
-    const history = useHistory();
+class ChooseOptions extends Component {
+    state = {
+        isLoading: false
+    }
 
-    const getGeneratedData = async (e) => {
-        console.log("in getGeneratedData")
+
+    getGeneratedData =async (e) => {
+        console.log("in getGeneratedData", this.props)
         const token = localStorage.getItem('token')
         const headers = {Authorization: `Bearer ${token}`}
         const username = localStorage.getItem('username')
         const datasetName = localStorage.getItem('lastUploadedFile')
         const modelType = 'CTGAN'
+        this.setState({isLoading:true})
         // const response = await axios.get(`http://104.248.137.182:5001/fit_generate/${username}/${modelType}/${datasetName}`,
         const response = await Axios.get(`datasets/get_generated/${username}/${modelType}/${datasetName}`,
-            { headers: {'Authorization': `Bearer ${token}`}})
-        console.log(response)
+            { headers})
+        //need to get the json report - call to the endpoint - is response.ok then storing the (json data) state in redux
+        //redirect to the charts
 
+        console.log(response)
+        this.setState({isLoading:false})
         if (response) {
-            history.push("/processing");
+           //
+            //history.push("/processing");
         }
     }
-    return (
-        <Body>
-            <FirstContainer>
-                <UploadBlock>
-                    <DragDropField>
-                        <UploadSuccess>
-                            <SuccessText>Your Dataset Was Uploaded Successfully</SuccessText>
-                            <SuccessSubText>Choose the Model Type and Number of Rows to be processed so that the
-                                synthetic data to be generated</SuccessSubText>
-                        </UploadSuccess>
-                        <ChooseModelRows>
-                            <ChooseModel>
-                                <MenuText>Select Model Type</MenuText>
-                                <DropDownMenu>
-                                    <option>Select...</option>
-                                    <option>CTGAN</option>
-                                    <option>TSGAN</option>
-                                    <option>OTHER METHODS SOON</option>
-                                </DropDownMenu>
-                            </ChooseModel>
-                            <ChooseRows>
-                                <MenuText>Choose Number of Rows</MenuText>
-                                <DropDownMenu>
-                                    <option>Select...</option>
-                                    <option>1000</option>
-                                    <option>5000</option>
-                                    <option>OTHER OPTIONS</option>
-                                </DropDownMenu>
-                            </ChooseRows>
-                        </ChooseModelRows>
-                        <WhiteButton type='submit' onClick={getGeneratedData} style={{marginBottom: "5%"}}>Start Syntheticus</WhiteButton>
-                    </DragDropField>
-                </UploadBlock>
-            </FirstContainer>
-        </Body>
-    )
+    render() {
+        return (
+            <Body>
+                {this.state.isLoading ? <Processing/> :
+                    <FirstContainer>
+                        <UploadBlock>
+                            <DragDropField>
+                                <UploadSuccess>
+                                    <SuccessText>Your Dataset Was Uploaded Successfully</SuccessText>
+                                    <SuccessSubText>Choose the Model Type and Number of Rows to be processed so that the
+                                        synthetic data to be generated</SuccessSubText>
+                                </UploadSuccess>
+                                <ChooseModelRows>
+                                    <ChooseModel>
+                                        <MenuText>Select Model Type</MenuText>
+                                        <DropDownMenu>
+                                            <option>Select...</option>
+                                            <option>CTGAN</option>
+                                            <option>TSGAN</option>
+                                            <option>OTHER METHODS SOON</option>
+                                        </DropDownMenu>
+                                    </ChooseModel>
+                                    <ChooseRows>
+                                        <MenuText>Choose Number of Rows</MenuText>
+                                        <DropDownMenu>
+                                            <option>Select...</option>
+                                            <option>1000</option>
+                                            <option>5000</option>
+                                            <option>OTHER OPTIONS</option>
+                                        </DropDownMenu>
+                                    </ChooseRows>
+                                </ChooseModelRows>
+                                <WhiteButton type='submit' onClick={this.getGeneratedData} style={{marginBottom: "5%"}}>Start
+                                    Syntheticus</WhiteButton>
+                            </DragDropField>
+                        </UploadBlock>
+                    </FirstContainer>
+                }
+            </Body>
+        )}
 
 }
 
